b2d1e268cc035d3fac210ea620c321ce9fc6571f
spacedude609 <jwmidds@outlook.com>
2021-06-28 10:44:34 -0400

    Stuff

--- src/main/java/net/mcreator/testmod/item/Kirby_OreIngotItem.java
+++ src/main/java/net/mcreator/testmod/item/Kirby_OreIngotItem.java
@@ -5,10 +5,10 @@ import net.minecraftforge.registries.ObjectHolder;
 
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.block.BlockState;
 
+import net.mcreator.testmod.itemgroup.CustomitemsItemGroup;
 import net.mcreator.testmod.TestModModElements;
 
 @TestModModElements.ModElement.Tag
@@ -25,7 +25,7 @@ public class Kirby_OreIngotItem extends TestModModElements.ModElement {
 	}
 	public static class ItemCustom extends Item {
 		public ItemCustom() {
-			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			super(new Item.Properties().group(CustomitemsItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
 			setRegistryName("kirby_ore_ingot");
 		}
 
